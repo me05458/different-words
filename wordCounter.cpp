e1913f9fe6d8b50441ce6bb1d9432901727419cf
me05458 <193655209+me05458@users.noreply.github.com>
2026-03-23 13:24:30 -0600

    started logic

--- wordCounter.cpp
+++ wordCounter.cpp
@@ -3,14 +3,99 @@
 #include "test.h"
 #include "w_file.h"
 #include <cstddef>
+#include <fstream>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
 
 int main() {
-    w_Test::testFile2();
-    //w_Test::testList();
-   // cout<<"hi"<<std::endl;
+/*    fstream vocabulary{"data.txt", ios::in | ios::out};
+    if(!vocabulary.is_open())
+    {
+        cout<<"File opening error occured"<<endl;
+        return 0;
+    }*/
+
+    w_String filename("data.txt",8);
+    w_File dataArea(filename);
+
+    printf("enter word(s) separated by spaces\n"
+        "or enter command (ex. '-help')\n");
+
+    char a_char = 0;
+    bool keepgo = true;
+    char* buff = new char[100];
+
+    while(true)
+    {
+        whilestart:
+        if(cin.peek()=='\n')
+        {
+            printf("imagine help here\n");
+            cin.get();
+        }
+        else if(cin.peek()=='-')
+        {
+            cin.get();
+            switch(cin.get())
+            {
+                case 'h':
+                    printf("imagine useful info here\n");
+                    break;
+                case 'q':
+                    printf("need to go! bye!\n");
+                    keepgo = false;
+                    break;
+                case 'l':
+                    printf("10 longest words displayed here\n");
+                    break;
+                case 'd':
+                    printf("10 most duplicated words displayed here\n");
+                    break;
+                    /* other commands here*/
+
+                case '\n':  //weird case of "-\n"
+                        cin.putback('\n');
+                default:
+                        printf("uncknown command; will ignore\n");
+                        break;
+            }
+            if(!keepgo)
+            {
+                break;
+            }
+            cin.ignore(100,'\n');
+        }
+        else
+        {
+            w_String entry("",0);
+            a_char = cin.get();
+            while(a_char!= ' ' && a_char!='\n')
+            {
+                if(a_char>='A' && a_char<='z')
+                {
+                    entry.append(a_char);
+                }
+                else
+                {
+                    printf("invalid word; will ignore\n");
+                    goto whilestart;
+                }
+                a_char=cin.get();
+            }
+            printf("word: ");
+            entry.printString();
+            printf("\n");
+            //int length = entry.size();
+            //sprintf(buffer,"%s,%d,%d\n",entry,entry.size(),1);
+            //entry.set(buffer);
+            dataArea.insert(0,entry,entry.size(),1);
+        }
+    }
+
 
 
 
